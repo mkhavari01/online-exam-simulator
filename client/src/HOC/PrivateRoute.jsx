f916dd0c255b1508e7382c1190fb4cc1bf8d97bf
Mahdi Khavari <mkhavari0120@gmail.com>
2023-04-25 09:26:21 +0430

    Update PrivateRoute.jsx

--- client/src/HOC/PrivateRoute.jsx
+++ client/src/HOC/PrivateRoute.jsx
@@ -1,23 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 import { checkToken } from "../redux/tokenSlice";
+import axios from "axios";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const isLoading = useSelector((state) => state.token.loading);
-  const response = useSelector((state) => state.token.isTokenValid);
+  // const isLoading = useSelector((state) => state.token.loading);
+  // const response = useSelector((state) => state.token.isTokenValid);
+
+  // const [res, setRes] = useState(false);
+
+  // console.log("res is", res);
+
+  // useEffect(() => {
+  //   dispatch(checkToken());
+  // }, []);
+  const [data, setData] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    dispatch(checkToken());
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          `${process.env.REACT_APP_BACKEND_URL}api/user/check-token`,
+          null,
+          {
+            headers: {
+              token: localStorage.getItem("token"),
+            },
+          }
+        );
+        setData(response.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
   }, []);
 
-  if (!response) {
-    return <Redirect to={{ pathname: "/login" }} />;
+  if (isLoading) {
+    return <div style={{ color: "black" }}>Loading...</div>;
   }
 
-  return <Route {...rest} render={(props) => <Component {...props} />} />;
+  if (data) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
+  }
+
+  if (error) {
+    return <div style={{ color: "black" }}>Error: {error.message}</div>;
+  }
 };
 
 export { PrivateRoute };
